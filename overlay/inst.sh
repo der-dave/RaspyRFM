6c6da66ffc187257923d7ff676bb836257445839
der-dave <der-dave@users.noreply.github.com>
2022-04-02 23:33:16 +0200

    apps deleted for local testing

--- overlay/inst.sh
+++
@@ -1,8 +0,0 @@
-dtc -@ -I dts -O dtb -o raspyrfm.dtbo raspyrfm.dts
-cp raspyrfm.dtbo /boot/overlays/
-echo "Comment this line in /boot/config.txt"
-echo "#dtparam=spi=on"
-echo "Add this line to /boot/config.txt"
-echo "dtoverlay=raspyrfm"
-
-
